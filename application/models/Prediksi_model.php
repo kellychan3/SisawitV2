1cc77e005c022f61dd05daf3628b0996d3957776
Kelly Chan <kelichen51257@gmail.com>
2025-06-11 10:23:53 +0700

    latest update

--- application/models/Prediksi_model.php
+++ application/models/Prediksi_model.php
@@ -3,18 +3,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Prediksi_model extends CI_Model
 {
+    private $dw;
+
     public function __construct()
     {
         parent::__construct();
+        $this->dw = $this->load->database('dw', TRUE); 
     }
 
     public function get_prediksi_by_year($tahun)
     {
-        $this->db->select('bulan, SUM(hasil_kg) as total_prediksi');
-        $this->db->from('hasil_prediksi');
-        $this->db->where('tahun', $tahun);
-        $this->db->group_by('bulan');
-        $query = $this->db->get();
+        $this->dw->select('bulan, SUM(hasil_kg) as total_prediksi');
+        $this->dw->from('fact_prediksi_panen');
+        $this->dw->where('tahun', $tahun);
+        $this->dw->group_by('bulan');
+        $query = $this->dw->get();
 
         $result = [];
         foreach ($query->result() as $row) {
@@ -25,45 +28,26 @@ class Prediksi_model extends CI_Model
     }
 
     public function get_aktual_by_year($tahun)
-    {
-        $token = $this->session->userdata('token');
-        if (!$token) return [];
+{
+    $this->dw->select('w.bulan, SUM(f.jumlah_panen) as total_panen');
+    $this->dw->from('fact_panen f');
+    $this->dw->join('dim_waktu w', 'f.sk_waktu = w.sk_waktu');
+    $this->dw->where('w.tahun', $tahun);
+    $this->dw->group_by('w.bulan');
 
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "http://103.150.101.10/api/pemanenan",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => [
-                "Authorization: Bearer $token",
-                "Accept: application/json"
-            ],
-        ]);
-
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        $data = json_decode($response, true);
-        if (!is_array($data)) return [];
-
-        $result = [];
-
-        foreach ($data as $item) {
-            $tanggal = date_create($item['tanggal_panen']);
-            if ((int)date_format($tanggal, 'Y') == $tahun) {
-                $bulan = (int)date_format($tanggal, 'n');
-                if (!isset($result[$bulan])) {
-                    $result[$bulan] = 0;
-                }
-                $result[$bulan] += (int)$item['jumlah_panen'];
-            }
-        }
-
-        return $result;
+    $query = $this->dw->get();
+    $result = [];
+    foreach ($query->result() as $row) {
+        $result[(int)$row->bulan] = (int)$row->total_panen;
     }
+
+    return $result;
+}
 
     public function get_tahun_list()
-    {
-        $query = $this->db->query("SELECT DISTINCT tahun FROM hasil_prediksi ORDER BY tahun");
-        return $query->result_array();
-    }
+{
+    $query = $this->dw->query("SELECT DISTINCT tahun FROM fact_prediksi_panen ORDER BY tahun");
+    return $query->result_array();
+}
+
 }
